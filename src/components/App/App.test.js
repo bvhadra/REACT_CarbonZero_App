a385495fd396bf45c53a2af4975727fc5b19ff02
gipo-ski <koladiji@gmail.com>
2023-06-12 14:06:44 +0100

    corrected errors in app.test.js, commiting to test Git Action

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -4,11 +4,5 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linklement).toBeInTheDocument();
-});
-
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByTxt(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
