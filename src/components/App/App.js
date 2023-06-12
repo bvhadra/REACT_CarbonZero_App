2ec6d855b64e01b57b5c9cdd3a0242f241a8d577
gipo-ski <koladiji@gmail.com>
2023-06-12 14:21:03 +0100

    testing my branch on github action

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          fLearn Rneact
         </a>
       </header>
     </div>
