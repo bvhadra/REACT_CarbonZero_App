8f59a0c58ce3e51b8c667f77014d226ba0737544
gipo-ski <koladiji@gmail.com>
2023-06-12 17:01:32 +0100

    added images to assets folder and corrected file paths to remove compilation errors

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
-import logo from '../../logo.svg';
+// import logo from '../../logo.svg';
+import Home from '../Home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Home />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Max Rules
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
