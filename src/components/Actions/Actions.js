2ecea0a30db9f66d5b7bfcae87a2dfb8a756df95
gipo-ski <koladiji@gmail.com>
2023-06-15 20:08:00 +0100

    finally got react-router to render our app navbar on rewards page, the ist page achieved! cool

--- src/components/Actions/Actions.js
+++ src/components/Actions/Actions.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar';
+import React from "react";
+import Navbar from "../Navbar/Navbar";
 
 export function Actions() {
-    return (
-      <>
-        <header>
-          <Navbar />
-        </header>
-        <h1>HELLO WORLD!</h1>
-        <div>Actions</div>
-      </>
-    );
+  return (
+    <>
+      <Navbar />
+      <h1>HELLO WORLD!</h1>
+      <div>Actions</div>
+    </>
+  );
 }
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,33 @@
-
 import { Route, Routes } from "react-router-dom";
 import Home from "../Home/Home";
 import Rewards from "../Rewards/Rewards";
-import Questionnaire from "../Questionnaire/Questionnaire";
-import UserComp from '../UserComp/usercomp';
-// import UserComp from '../UserComp/usercomp';
+import BecomeAMember from "../BecomeAMember/BecomeAMember";
+import SignIn from "../SignIn/SignIn";
+import Questionnaire from "../UserComp/usercomp";
+import UserComp from "../UserComp/usercomp";
 import { Actions } from "../Actions/Actions";
 import "./App.css";
 
-
-
-
 function App() {
+  const paths = {
+    "/": "",
+    "/rewards": "/Rewards/Rewards.js",
+    "/questionnaire": "/Questionnaire/Questionnaire",
+    "/become a member": "/Become%20a%20member/Become%20a%20member.js",
+    "/sign in": "/Sign%20in/Sign%20in.js",
+    "/usercomp": "",
+    "/actions": ""
+  };
+ 
   return (
+
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/rewards" element={<Rewards />} />
-        <Route path="/quetionnaire" element={<Questionnaire />} />
+        <Route path={paths["/"]} element={<Home />} />
+        <Route path={paths["/rewards"]} element={<Rewards />} />
+        <Route path="/questionnaire" element={<Questionnaire />} />
+        <Route path="/become a member" element={<BecomeAMember />} />
+        <Route path="/sign in" element={<SignIn />} />
         <Route path="/usercomp" element={<UserComp />} />
         <Route path="/actions" element={<Actions />} />
       </Routes>
