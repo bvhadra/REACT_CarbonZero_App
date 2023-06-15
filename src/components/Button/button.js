5324dbe756d6777c051629bba3e8b6178c53dbc0
Arwa <124366130+arwa-omran@users.noreply.github.com>
2023-06-15 15:42:26 +0100

    no more error messages on last question

--- src/components/Button/button.js
+++ src/components/Button/button.js
@@ -1,11 +1,14 @@
 import React from "react";
+import questionsList from "../../lib/data";
 
 export default function Button({ data, questionIndex, setQuestionIndex, response, setResponse }) {
   const { answers, question, calculations } = data;
 //   console.log(calculations);
 
   const handleClick = (index) => {
+    if (questionIndex !== questionsList.length - 1) {
     setQuestionIndex(questionIndex + 1);
+    }
     // setResponse([...response, calculations[index]]);
    setResponse(response.map((item, idx) => (idx === questionIndex ? { ...item, response: calculations[index] }  : item)));
     console.log(response);
