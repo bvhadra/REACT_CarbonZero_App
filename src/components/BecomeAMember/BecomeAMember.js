536b5f12a5206a1bd9ce69082cae1fd6fbb2bc5b
bidhan-vhadra <bvhadra@gmail.com>
2023-06-22 12:04:16 +0100

    added page and CSS file for sign up to become a member (without form)

--- src/components/BecomeAMember/BecomeAMember.js
+++ src/components/BecomeAMember/BecomeAMember.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar';
-
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import "./BecomeAMember.css"
+import { Link } from "react-router-dom";
 
 export default function BecomeAMember() {
-    return (
-        <>
-            <Navbar/>
-        <div>BecomeAMember</div>
-      </>
-    );
+  return (
+    <>
+      <Navbar />
+      <main className="become-a-member-main">
+        <h1>Become A Member</h1>
+        <div className="white-box">
+          <h2>Sign up here to get access to your account, profile and use our app!</h2>
+        </div>
+        <button className="register">
+          <Link to="../BecomeAMember" id="become-a-member">
+            Register
+          </Link>
+        </button>
+      </main>
+    </>
+  );
 }
+
+
+
+
+
