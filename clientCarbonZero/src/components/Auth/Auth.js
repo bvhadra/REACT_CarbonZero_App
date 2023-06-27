7063b4f7ba9d46de531ac2f85c7af44fc06aa172
gipo-ski <koladiji@gmail.com>
2023-06-27 17:06:38 +0100

    added custom inline css to Auth.js, couldn't work out how to use the inbuilt styles in Supabase!

--- clientCarbonZero/src/components/Auth/Auth.js
+++ clientCarbonZero/src/components/Auth/Auth.js
@@ -27,7 +27,37 @@ export default function App() {
   }, []);
 
   if (!session) {
-    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+    return (
+      <Auth
+        supabaseClient={supabase}
+        appearance={{
+          style: {
+            container: { height: "40%", width: "40%", margin: "0 auto" },
+            button: {
+              background: "#29524a",
+              color: "white",
+              marginTop: "20px",
+              width: "100%",
+              height: "70px",
+              alignItems: "center",
+            },
+            anchor: { color: "#000000", textDecoration: "none" },
+            divider: { color: "green" },
+            label: { fontSize: "20px", fontWeight: "300", color: "#000000" },
+            input: {
+              height: "40px",
+              width: "100%",
+              color: "green",
+              padding: "10px",
+              fontSize: "20px",
+              fontWeight: "300",
+            },
+            loader: { color: "green" },
+            message: { color: "green" },
+          },
+        }}
+      />
+    );
   } else {
     return <div>Logged in!</div>;
   }
