a05ffdb5cc1b5516705688c1cd33279177fd2b4b
M4x <maxmurray1999@gmail.com>
2023-06-29 16:40:47 +0100

    updated node version

--- clientCarbonZero/src/components/ProfileResults/ProfileResults.js
+++ clientCarbonZero/src/components/ProfileResults/ProfileResults.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Paper from '@mui/material/Paper';
 import './ProfileResults.css';
 
